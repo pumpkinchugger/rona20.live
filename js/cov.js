66a5b4787e57f5973d0b251e82370299c9fc88ea
Rafferty <raffertywwong@gmail.com>
2020-06-01 01:35:57 -0400

    fixed real-time data / added donation popup

--- js/cov.js
+++ js/cov.js
@@ -1,66 +1,67 @@
-// create an async function to handle promised based execution order
-(async()=>{
-    // define some globals
-  var update = async()=>{
+// popup window function
+function togglePopup(){
+  document.getElementById("popup-1").classList.toggle("active");
+}
 
+// create an async function to handle promised based execution order
+(async() => {
+  // define some globals
+  var update = async(text, updateTime) => {
     // download the data
     console.log('Report: Download Started');
-    var url = 'https://cov19.cc/report.json?v='+Math.random();
-    var res = await fetch(url);
-    var report = await res.json();
-    
+    var url = 'https://cov19.cc/report.json?v=' + Math.random();
+    try {
+      var res = await fetch(url);
+      var report = await res.json();
+    } catch (err) {
+      console.log(err)
+    }
+
     // set the last updated time
-    $('#last_updated').text(moment.utc(report.last_updated).fromNow());
+    document.getElementById('last_updated').innerHTML = moment.utc(report.last_updated).fromNow();
 
-    // get variable data
-    var world = report.regions.world;
+    var world = report.regions.world.totals;
+    // adding totals
+    document.getElementById('total_confirmed').textContent = world.confirmed.toLocaleString('en-US')
+    document.getElementById('total_deaths').textContent = world.deaths.toLocaleString('en-US')
+    document.getElementById('total_critical').textContent = world.critical.toLocaleString('en-US')
+    document.getElementById('total_recovered').textContent = world.recovered.toLocaleString('en-US')
+    document.getElementById('total_active').textContent = (world.confirmed - (world.deaths + world.recovered)).toLocaleString('en-US')
 
-    var total_confirmed = report.regions.world.totals.confirmed;
-    document.getElementById("total_confirmed").innerHTML = total_confirmed.commaSplit();
-
-    var total_critical = world.totals.critical;
-    document.getElementById("total_critical").innerHTML = total_critical.commaSplit();
-
-    var total_deaths = world.totals.deaths;
-    document.getElementById("total_deaths").innerHTML = total_deaths.commaSplit();
-
-    var total_active = world.totals.confirmed - (world.totals.deaths + world.totals.recovered);
-    document.getElementById("total_active").innerHTML = total_active.commaSplit();
-
-    var total_recovered = world.totals.recovered;
-    document.getElementById("total_recovered").innerHTML = total_recovered.commaSplit();
-
+    // set data list
+    //document.getElementById('data_container').innerHTML = createHTMLTemplate(report.regions.world.totals)
     // hide the loading icon
     $('#loader').hide();
-    };
+  };
 
-    // store last updated date
-    var old_last_updated;
+  // store last updated date
+  var old_last_updated;
 
-    // check for the last update
-    var update_check = async()=>{
+  // check for the last update
+  var update_check = async() => {
     console.log('Checking for updates');
     var res = await fetch('https://cov19.cc/last_updated.txt');
     var last_updated = await res.text();
-        
+
     // if the last updated date is newer than the stored last updated date then update the variable and update the table with the new data
-    if(old_last_updated == last_updated)return;
+    // if (old_last_updated == last_updated) return;
     old_last_updated = last_updated;
 
-    update();
-    };
+    update(last_updated, Date.now());
+  };
 
-    // initialize
-    update_check();
+  // initialize
+  update_check();
 
-    // check for updates every 60 seconds
-    setInterval(update_check, 60000);
+  // check for updates every 10 seconds
+  setInterval(update_check, 10000);
 })();
 
-    //cov19 prototypes
-    String.prototype.commaSplit = function() {
-    return this.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    };
-    Number.prototype.commaSplit = String.prototype.commaSplit;
-
-
+function createHTMLTemplate(data) {
+  let html = ''
+  for (let key in data) {
+    html += `<div>${key}: ${data[key].toLocaleString('en-US')}</div>`
+  }
+  html += `<div>active: ${(data.confirmed - (data.deaths + data.recovered)).toLocaleString('en-US')}</div>`
+  return html
+}
