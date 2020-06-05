cad85dfdb1e9a18e70c923a4ff5ec986da1ff5f3
Rafferty <raffertywwong@gmail.com>
2020-06-05 00:57:25 -0400

    implement country search/paypal

--- js/cov.js
+++ js/cov.js
@@ -3,12 +3,82 @@ function togglePopup(){
   document.getElementById("popup-1").classList.toggle("active");
 }
 
+// fill country data menu
+var fillCountryMenu = async() => {
+  // get report data
+  var url = 'https://cov19.cc/report.json?v=' + Math.random();
+  try {
+    var res = await fetch(url);
+    var report = await res.json();
+  } catch (err) {
+    console.log(err)
+  }
+
+  // fill select menu with countries
+  var world_list = report.regions.world.list;
+  for(var i in world_list){
+  var countryObject = world_list[i];
+  var x = JSON.stringify(countryObject.country);
+  x = x.replace(/^"|"$/g, '');
+  var o = new Option(x, i);
+  $(o).html(x);
+  $("select").append(o);
+  }
+
+  // fill default selected country info
+  getCountryInfo();
+}
+
+// get country info when changed
+var getCountryInfo = async() => {
+  // get report data
+  var url = 'https://cov19.cc/report.json?v=' + Math.random();
+  try {
+    var res = await fetch(url);
+    var report = await res.json();
+  } catch (err) {
+    console.log(err)
+  }
+
+  var world_list = report.regions.world.list;
+
+  // remove previous flag
+  document.getElementById("countryFlag").innerHTML = ``;
+  // filling select menu
+  var d = document.getElementById("dropView");
+  var userOpt = d.options[d.selectedIndex].text;
+  for(var i in world_list){
+    var countryObject = world_list[i];
+    if(countryObject.country == userOpt){
+      document.getElementById("countryName").innerText = userOpt;
+      $('#countryFlag').prepend(`
+        <i class="flag-icon flag-icon-${countryObject.country_code} rounded">
+      `);
+      document.getElementById("country_confirmed").textContent = countryObject.confirmed.toLocaleString('en-US')
+      document.getElementById("country_deaths").textContent = countryObject.deaths.toLocaleString("en-US")
+      document.getElementById("country_recovered").textContent = countryObject.recovered.toLocaleString("en-US")
+      document.getElementById("country_active").textContent = (countryObject.confirmed - (countryObject.deaths + countryObject.recovered)).toLocaleString('en-US')
+      document.getElementById("country_critical").textContent = countryObject.critical.toLocaleString("en-US")
+      document.getElementById("country_tests").textContent = countryObject.tests.toLocaleString("en-US")
+      var incidenceNum = parseFloat(countryObject.Incidence_Rate);
+      var roundedIncidence = incidenceNum.toFixed(2);
+      document.getElementById("country_incidence_rate").textContent = roundedIncidence;
+      var fatalityRatio = parseFloat(countryObject["Case-Fatality_Ratio"])
+      var roundedFatalityRatio = fatalityRatio.toFixed(2);
+      document.getElementById("country_fatality_ratio").textContent =  roundedFatalityRatio;
+    }
+  }
+}
+
 // create an async function to handle promised based execution order
 (async() => {
+  // populate select country menu once
+  fillCountryMenu();
+
   // define some globals
   var update = async(text, updateTime) => {
     // download the data
-    console.log('Report: Download Started');
+    // console.log('Report: Download Started');
     var url = 'https://cov19.cc/report.json?v=' + Math.random();
     try {
       var res = await fetch(url);
@@ -30,6 +100,7 @@ function togglePopup(){
 
     // set data list
     //document.getElementById('data_container').innerHTML = createHTMLTemplate(report.regions.world.totals)
+   
     // hide the loading icon
     $('#loader').hide();
   };
@@ -39,7 +110,7 @@ function togglePopup(){
 
   // check for the last update
   var update_check = async() => {
-    console.log('Checking for updates');
+    //console.log('Checking for updates');
     var res = await fetch('https://cov19.cc/last_updated.txt');
     var last_updated = await res.text();
 
@@ -53,8 +124,8 @@ function togglePopup(){
   // initialize
   update_check();
 
-  // check for updates every 10 seconds
-  setInterval(update_check, 10000);
+  // check for updates every 60 seconds
+  setInterval(update_check, 60000);
 })();
 
 function createHTMLTemplate(data) {
